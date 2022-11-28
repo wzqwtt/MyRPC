24ef2d1d2e6d9757b74db92af83135acb919df2a
wzqwtt <1722249371@qq.com>
2022-11-28 14:38:13 +0800

    工程初始化

---
+++ rpc-consumer/src/test/java/com/wzq/rpc/rpcconsumer/RpcConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzq.rpc.rpcconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ rpc-provider/src/main/java/com/wzq/rpc/rpcprovider/RpcProviderApplication.java
@@ -0,0 +1,13 @@
+package com.wzq.rpc.rpcprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpcProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RpcProviderApplication.class, args);
+    }
+
+}
